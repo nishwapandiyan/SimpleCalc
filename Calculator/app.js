c8b8c00a59b128fc311aa87ce995c82078f8b4dd
R.P.Nishwa <nishwapandiyan@gmail.com>
2026-01-28 10:05:01 +0530

    Add files via upload

---
+++ Calculator/app.js
@@ -0,0 +1,22 @@
+display = document.getElementById("display");
+
+function appendToDisplay(input){
+    display.value += input;
+}
+
+function clearDisplay(){
+    display.value = "";
+}
+
+function clearLast(){
+    display.value = display.value.slice(0,-1);
+}
+
+function calculate(){
+    try{
+    display.value = eval(display.value);
+    }
+    catch(error){
+        display.value = "Syntax Error";
+    }
+}
